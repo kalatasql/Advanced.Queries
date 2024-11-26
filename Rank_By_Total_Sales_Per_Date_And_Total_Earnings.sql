4560e9486a5a4592333c76d4d347644ea39a9bdb
kalatasql <kalata.sql@gmail.com>
2024-11-26 13:42:47 +0200

    Update Rank_By_Total_Sales_Per_Date_And_Total_Earnings.sql

--- Rank_By_Total_Sales_Per_Date_And_Total_Earnings.sql
+++ Rank_By_Total_Sales_Per_Date_And_Total_Earnings.sql
@@ -1,9 +1,9 @@
 SELECT 
-		RANK() OVER (ORDER BY COUNT(*) DESC) AS [Rank Counter], -- Get counted rows from the bigger to smaller data and show ranks (1,2,3.....1000)
-		COUNT(*) AS [Total Sales], -- Counting sales for each date
-    SUM(Product_Price) AS [Total Earnings], -- Sum Total Earnings for each date
-		LOADDT
-FROM Sales
+      RANK() OVER (ORDER BY COUNT(*) DESC) AS [Rank Counter], -- Get counted rows from the bigger to smaller data and show ranks (1,2,3.....1000)
+      COUNT(*) AS [Total Sales], -- Counting sales for each date
+      SUM(Product_Price) AS [Total Earnings], -- Sum Total Earnings for each date
+      LOADDT
+FROM  Sales
 WHERE LOADDT >= '2024-01-01' -- Get all dates after January 1, 2024 year
 GROUP BY LOADDT -- Grouping rows by load date
 ORDER BY [RANK COUNTER] -- Show rank orders from 1 to max
