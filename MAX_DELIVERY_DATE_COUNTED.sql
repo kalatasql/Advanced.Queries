e9cdcd5c1d633711f5fdfc3d5c2045bf9bcfcc81
kalatasql <kalata.sql@gmail.com>
2024-11-27 22:27:03 +0200

    Create MAX_DELIVERY_DATE_COUNTED.sql

---
+++ MAX_DELIVERY_DATE_COUNTED.sql
@@ -0,0 +1,13 @@
+	SELECT
+		  COUNT(*),
+		  [Delivery Date]
+	FROM
+	(SELECT 
+
+				RANK() OVER (ORDER BY [DELIVERY DATE] DESC) AS RNK,
+				[Delivery Date]
+
+	FROM OpenOrders 
+	) TBL
+	WHERE RNK = 1
+	group by [delivery date]
