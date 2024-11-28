b0434b32920ac95a5e2cd318fddf18cb4a3b254a
kalatasql <kalata.sql@gmail.com>
2024-11-28 14:28:09 +0200

    Update MAX_DELIVERY_DATE_COUNTED.sql

--- MAX_DELIVERY_DATE_COUNTED.sql
+++ MAX_DELIVERY_DATE_COUNTED.sql
@@ -1,13 +1,8 @@
-	SELECT
-		  COUNT(*),
-		  [Delivery Date]
-	FROM
-	(SELECT 
-
-				RANK() OVER (ORDER BY [DELIVERY DATE] DESC) AS RNK,
-				[Delivery Date]
-
-	FROM OpenOrders 
-	) TBL
-	WHERE RNK = 1
-	group by [delivery date]
+SELECT COUNT(*),
+       [Delivery Date]
+FROM
+    (SELECT RANK() OVER (ORDER BY [DELIVERY DATE] DESC) AS RNK,
+	    [Delivery Date]
+     FROM OpenOrders ) TBL
+WHERE RNK = 1
+group by [delivery date]
